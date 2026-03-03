2df81b053cd7e7556e0220a8accb2e0be70c1179
Prachi Thorat <156530093+Prachi-1716@users.noreply.github.com>
2026-03-03 12:59:30 -0500

    Update authController.js

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -85,7 +85,7 @@ module.exports.signUp = async (req, res, next) => {
     res.cookie("token", acces_token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "None",
         maxAge: 3 * 24 * 60 * 60 * 1000             
     });
     req.user = user;
@@ -109,7 +109,7 @@ module.exports.signIn = async(req, res, next)=>{
     res.cookie("token", acces_token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "None",
         maxAge: 3 * 24 * 60 * 60 * 1000             
     });
     req.user = user;
@@ -121,7 +121,7 @@ module.exports.logout = (req, res) => {
     res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax"
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "None"
 });
 
     return res.status(200).json({ message: "Logged out successfully" });
@@ -166,7 +166,7 @@ module.exports.googleAuth = async(req, res)=>{
     res.cookie("token", acces_token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "None",
         maxAge: 3 * 24 * 60 * 60 * 1000
     });
 
